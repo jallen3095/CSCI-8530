e5255343bc6cf3ba02db185909686a4d2307414e
Nick Palacio <npalacio2240@gmail.com>
2019-04-14 16:38:22 -0500

    Modified to use temp sensor

--- Project/Project.ino
+++ Project/Project.ino
@@ -2,24 +2,37 @@
 #include <stdio.h>
 
 int buttonPin = 2;
+int tempPin = 3;
 rgb_lcd lcd;
-int buttonPushedCount = 0;
- 
+
 void setup() {
-  pinMode(buttonPin,INPUT);
+  pinMode(buttonPin, INPUT);
   lcd.begin(16, 2);
+  lcd.setRGB(0, 0, 255);
   lcd.print("Push the button");
-  lcd.setCursor(0,1);
-  lcd.print("to change colors");
+  lcd.setCursor(0, 1);
+  lcd.print("to see temp");
 }
 
 void loop() {
-  if(digitalRead(buttonPin))//check button is pressed or not
-  {
-    lcd.setRGB(255, 0, 0);
+  if (digitalRead(buttonPin)) {
+    float temp = getTemp();
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    lcd.print(temp, 2);
+    lcd.print("      ");
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.print("Push the button");
+    lcd.setCursor(0, 1);
+    lcd.print("to see temp");
   }
- else
- {
-    lcd.setRGB(0, 0, 255);
- }
+}
+
+float getTemp() {
+    float temp = analogRead(tempPin);
+    temp = (float)(1023 - temp) * 10000 / temp;
+    return 1 / (log(temp / 10000) / 3975 + 1 / 298.15) - 273.15;  
 }
