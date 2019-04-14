7b57e90e9e93eabfb115abcecd673e226a62b0a9
Nick Palacio <npalacio2240@gmail.com>
2019-04-14 16:09:06 -0500

    Working version of project with updated instructions

---
+++ Project/Project.ino
@@ -0,0 +1,25 @@
+#include <rgb_lcd.h>
+#include <stdio.h>
+
+int buttonPin = 2;
+rgb_lcd lcd;
+int buttonPushedCount = 0;
+ 
+void setup() {
+  pinMode(buttonPin,INPUT);
+  lcd.begin(16, 2);
+  lcd.print("Push the button");
+  lcd.setCursor(0,1);
+  lcd.print("to change colors");
+}
+
+void loop() {
+  if(digitalRead(buttonPin))//check button is pressed or not
+  {
+    lcd.setRGB(255, 0, 0);
+  }
+ else
+ {
+    lcd.setRGB(0, 0, 255);
+ }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Project for CSCI-8530
+# Project for CSCI-8530
+
+## Getting Started:
+- Go through the directions in ArduinoSetupSteps.txt
+- Open the **Project/Project.ino** file in the Arduino IDE on your machine
+- Add any external libs located in the **Libs/** folder
+    - In the Arduino IDE go to Sketch --> Include Library --> Add Zip Library
+    - Select any ZIP files in the **Libs/** folder
